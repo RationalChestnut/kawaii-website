3316340650ded664929122d7cb766e83d0e131d4
Brian Mason <brian.kw.mason@gmail.com>
2022-12-01 16:56:00 -0500

    first commit

---
+++ src/Artwork/Artwork.page.js
@@ -0,0 +1,5 @@
+import React from "react";
+import styles from "./Artwork.module.css";
+export const Artwork = () => {
+  return <div className={styles.artworkContainer}>Artwork</div>;
+};
---
+++ src/Landing/Landing.page.js
@@ -0,0 +1,5 @@
+import React from "react";
+import styles from "./Landing.module.css";
+export const Landing = () => {
+  return <div className={styles.landingContainer}>Landing</div>;
+};
