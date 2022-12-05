74a06e16dca9d96e3f0f4f652a1e872f45127b00
Brian Mason <brian.kw.mason@gmail.com>
2022-12-05 17:24:33 -0500

    Added image

--- src/Landing/Landing.page.js
+++ src/Landing/Landing.page.js
@@ -1,5 +1,22 @@
 import React from "react";
 import styles from "./Landing.module.css";
+import landing from "../assets/landing.png";
 export const Landing = () => {
-  return <div className={styles.landingContainer}>Landing</div>;
+  return (
+    <div className={styles.landingContainer}>
+      <div className={styles.landingLeft}>
+        <img
+          src={landing}
+          alt="Cute anime girl"
+          className={styles.landingImage}
+        />
+      </div>
+      <div className={styles.landingRight}>
+        <h1 className={styles.landingTitle}>Kawaii</h1>
+        <p className={styles.landingDescription}>
+          Generate high quality anime images with AI.
+        </p>
+      </div>
+    </div>
+  );
 };
