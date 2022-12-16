7b01fb4f607fffe88c58dff8cd0903fef8afc7ff
Brian Mason <brian.kw.mason@gmail.com>
2022-12-15 21:09:31 -0500

    Landing page done

--- src/Landing/Landing.page.js
+++ src/Landing/Landing.page.js
@@ -1,21 +1,42 @@
 import React from "react";
+
 import styles from "./Landing.module.css";
-import landing from "../assets/landing.png";
+import arrow from "../assets/arrow.png";
+import appStore from "../assets/svgs/appstore.svg";
+import googlePlay from "../assets/svgs/googleplay.svg";
+import profile from "../assets/profile.png";
+
 export const Landing = () => {
   return (
-    <div className={styles.landingContainer}>
-      <div className={styles.landingLeft}>
-        <img
-          src={landing}
-          alt="Cute anime girl"
-          className={styles.landingImage}
-        />
-      </div>
-      <div className={styles.landingRight}>
-        <h1 className={styles.landingTitle}>Kawaii</h1>
-        <p className={styles.landingDescription}>
-          Generate high quality anime images with AI.
-        </p>
+    <div className={styles.homePageContainer}>
+      <div className={styles.contentContainer}>
+        <div className={styles.left}>
+          <div className={styles.content}>
+            <h1 className={styles.header}>Kawaii</h1>
+            <h2 className={styles.subHeader}>
+              Generate high quality anime art images
+            </h2>
+            <div className={styles.downloadImages}>
+              <a href="https://apps.apple.com/us/app/kawaii-ai-image-generator/id6444872562">
+                <img
+                  src={appStore}
+                  alt="Download on app store"
+                  className={styles.download}
+                />
+              </a>
+              <a href="https://play.google.com/store/apps/details?id=com.brianmason101.waifu.maker">
+                <img
+                  src={googlePlay}
+                  alt="Download on app store"
+                  className={styles.download}
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className={styles.right}>
+          <img src={profile} alt="App home page" className={styles.phone} />
+        </div>
       </div>
     </div>
   );
