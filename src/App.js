03aafb4d476693532420477adfcfdf95c0d931d0
Brian Mason <brian.kw.mason@gmail.com>
2023-03-07 17:52:40 -0500

    Removed things

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import { useEffect } from "react";
-import { isAndroid, isIOS } from "react-device-detect";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { MainNavigator } from "./MainNavigator";
 import { PrivacyPolicy } from "./PrivacyPolicy/PrivacyPolicy.page";
 import { Terms } from "./Terms/Terms";
 
 function App() {
-  useEffect(() => {
-    if (isAndroid) {
-      window.location.href =
-        "https://play.google.com/store/apps/details?id=com.brianmason101.waifu.maker";
-    } else if (isIOS) {
-      window.location.href =
-        "https://apps.apple.com/us/app/kawaii-ai-image-generator/id6444872562";
-    }
-  }, []);
-
   return (
     <Router>
       <Routes>
